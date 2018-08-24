e422e5db8e145f109a32bdcc7b7b5011ac0c3010
joaocosta12 <joao_rln27@hotmail.com>
2018-08-24 18:21:12 -0300

    first commit

---
+++ CMakeFiles/Asteroids.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Asteroids.dir/src/main.cpp.o"
+  "Asteroids.pdb"
+  "Asteroids"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Asteroids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ CMakeFiles/ColorCloud.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ColorCloud.dir/src/main.cpp.o"
+  "ColorCloud.pdb"
+  "ColorCloud"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ColorCloud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
